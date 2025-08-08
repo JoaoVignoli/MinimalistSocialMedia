655b76cadaf027ffa060f41bd5fad0bec661012a
JoaoVignoli <vignolijoao@gmail.com>
2025-08-08 14:19:58 -0300

    Criada entidade dos comentários dos Posts.

--- minimal-social-media/src/main/java/br/com/vignoli/minimal_social_media/Entities/Comment.java
+++ minimal-social-media/src/main/java/br/com/vignoli/minimal_social_media/Entities/Comment.java
@@ -1,5 +1,6 @@
-package main.java.br.com.vignoli.minimal_social_media.Entities;
+package br.com.vignoli.minimal_social_media.Entities;
 
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,4 +16,45 @@ public class Comment {
     @JoinColumn(name = "post_id")
     private Post post;
     private LocalDateTime createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
