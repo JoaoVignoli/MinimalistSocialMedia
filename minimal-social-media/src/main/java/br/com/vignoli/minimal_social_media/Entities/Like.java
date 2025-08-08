928541016db114ebff6f2b263b9b70e019143e72
JoaoVignoli <vignolijoao@gmail.com>
2025-08-08 14:19:18 -0300

    Criada entidade do Like dos Posts, foi necessário trocar o nome da tabela no DB, por conta de LIKE ser uma palavra reservada do SGDB.

--- minimal-social-media/src/main/java/br/com/vignoli/minimal_social_media/Entities/Like.java
+++ minimal-social-media/src/main/java/br/com/vignoli/minimal_social_media/Entities/Like.java
@@ -1,6 +1,10 @@
-package main.java.br.com.vignoli.minimal_social_media.Entities;
+package br.com.vignoli.minimal_social_media.Entities;
+
+import jakarta.persistence.*;
 
 @Entity
+// Necessário renomear a tabela, por conta que Like é utilizado no SQL. Estava causando erros nas consultas.
+@Table(name = "post_likes")
 public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,5 +15,29 @@ public class Like {
     @ManyToOne()
     @JoinColumn(name = "post_id")
     private Post post;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 
 }
